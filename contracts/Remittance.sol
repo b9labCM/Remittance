8723d29bff342450738e0ebea452fa3340dae07c
b9labCM <b9lab.mtlcrl@outlook.it>
2018-08-28 19:34:40 +0200

    saving gas

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -5,58 +5,50 @@ import "./Stoppable.sol";
 
 contract Remittance is Stoppable {
     
-    
     struct Deposit {
         uint amount; // Amount to send 
         uint timeLimit; // Time limit within the amount can be withdraw (not used for now)
-        address exchanger; 
-        
     }
-    
     
     mapping(bytes32 => Deposit) public deposits; 
     
-    event LogDeposit(address sender, address exchanger, uint amount, bytes32 psw);
-    event LogWithdraw(address exchanger, address receiver, uint amount);
+    event LogDeposit(address sender, uint amount, bytes32 psw);
+    event LogWithdraw(address receiver, uint amount);
     
     constructor () public { }
     
-    
-     // Function to load ether on contract. Once it is loaded the amount can 
-     //  be withdraw only to whom who knows the complete password 
-    
-    function depositOnContract(address exchanger, bytes32 puzzle) public payable onlyIfRunning  returns(bool success){
+    function depositOnContract( bytes32 puzzle) public payable onlyIfRunning  returns(bool success){
         require(msg.value > 0, "Insufficient funds");
-        require(msg.sender != address(0), " address must not be 0x0");
+        require(msg.sender != address(0), "Address must not be 0x0");
         require(deposits[puzzle].amount == 0, "Puzzle already used!");
-      
-        emit LogDeposit(msg.sender, exchanger, msg.value, puzzle);
+        
+        uint expiringTime = now + 500; 
+        
+        emit LogDeposit(msg.sender, msg.value, puzzle);
         deposits[puzzle] = Deposit({
     				amount: msg.value,
-    				timeLimit: now,
-    				exchanger: exchanger
+    				timeLimit: expiringTime
 				});
         return true; 
     }
 
-
-        
     function withdraw(string password1, string password2) public onlyIfRunning returns(bool success){
+        
         // you can withdraw coins only if you know the right password 
-        bytes32 tmp_password = computeHash(password1, password2);
+        bytes32 tmp_password = computeHash(password1, password2, msg.sender);
         
         uint tmpAmount = deposits[tmp_password].amount;
         require(tmpAmount != 0, "Thou shalt not pass!" ); // Only when both passwords are correct does the contract yield the Ether to Carol.
 	deposits[tmp_password].amount = 0;
-	
-	address tmpExchanger = deposits[tmp_password].exchanger;
-	require(msg.sender == tmpExchanger); // Carol submits both passwords to Alice's remittance contract (Only Carol can withdraw funds)
         
-        emit LogWithdraw(tmpExchanger, msg.sender,tmpAmount);
+        // require( now <= deposits[tmp_password].timeLimit, "Session expired");
+        
+        emit LogWithdraw(msg.sender,tmpAmount);
         msg.sender.transfer(tmpAmount);
         
         return true;
     }
+    
      
     // add a kill switch to the whole contract
     function killSwitch() public onlyOwner {
@@ -66,7 +58,7 @@ contract Remittance is Stoppable {
     /* Utils */
         
     // Used only for debugging to compute hash in a fast way 
-    function computeHash(string password1, string password2) public pure returns(bytes32){
-        return keccak256(abi.encodePacked(password1,password2));
+    function computeHash(string password1, string password2, address exchanger) public pure returns(bytes32){
+        return keccak256(abi.encodePacked(password1,password2, exchanger));
     }
 }  
