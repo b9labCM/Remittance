f3bf973375db6540140d56adf6c67cfce9a4772c
b9labCM <b9lab.mtlcrl@outlook.it>
2018-08-29 17:32:35 +0200

    test fixes

--- test/remittance_test.js
+++ test/remittance_test.js
@@ -1,7 +1,7 @@
 const Remittance = artifacts.require("./Remittance.sol");
 
 contract('Remittance', function(accounts){
-    let contract ; 
+    let remi; 
     const owner = accounts[0]; 
     const account_A = accounts[1]; 
     const account_B = accounts[2];
@@ -10,8 +10,8 @@ contract('Remittance', function(accounts){
     let pass1 = 'b';
     let pass2 = 'b';
 
-    let passHashed = '0xb8a68323ff350f076062861482bede9ffafb1c8dab43874c9558ce36c0da7124';
-  
+    let duration = 150;
+    let passHashed;
     
     /* Deploy a new contract for each test */
     beforeEach(async function(){
@@ -29,50 +29,76 @@ contract('Remittance', function(accounts){
 
     /*Test 2: Deposit creation*/
     it("Testing deposit creation", function() {
-        return remi.depositOnContract(account_C, passHashed, {from: account_A, value: web3.toWei(0.001, "ether")})
-        .then(txObj => {
+	return remi.computeHash(account_A, pass1, pass2, account_C)
+	.then( _passHashed => {
+		passHashed = _passHashed;
+		return remi.depositOnContract(passHashed, duration, {from: account_A, value: web3.toWei(0.0001, "ether")})
+	}).then(txObj => {
              assert.equal(txObj.receipt.status, 1, "Deposit fails ");
-             assert.equal(txObj.logs.length,1, "event has not been emitted");
-             assert.equal(txObj.logs[0].args.exchanger, account_C, "exchanger not set correctly")
+             assert.equal(txObj.logs.length,1, "Event has not been emitted");
+             assert.equal(txObj.logs[0].args.psw, passHashed, "puzzle not set correctly")
         })
 
     });
 
     /*Test 2: Deposit storage is correct*/
     it("Testing storage", function() {
-        return remi.depositOnContract(account_C, passHashed, {from: account_A, value: web3.toWei(0.001, "ether")})
-        .then(txObj => {
+        return remi.computeHash(account_A, pass1, pass2, account_C)
+	.then( _passHashed => {
+		passHashed = _passHashed;
+		return remi.depositOnContract(passHashed, duration, {from: account_A, value: web3.toWei(0.0001, "ether")})
+	}).then(txObj => {
              assert.equal(txObj.receipt.status, 1, "Deposit fails ");
-             assert.equal(txObj.logs.length,1, "event has not been emitted");
-             assert.equal(txObj.logs[0].args.exchanger, account_C, "exchanger not set correctly")
-	     assert.equal(txObj.logs[0].args.amount.toString(10), web3.toWei(0.001, "ether").toString(10), "exchanger not set correctly")
+             assert.equal(txObj.logs.length,1, "Event has not been emitted");
+	     assert.equal(txObj.logs[0].args.amount.toString(10), web3.toWei(0.0001, "ether").toString(10), "Amount not set correctly")
 	     return remi.deposits(passHashed)
 	}).then(dep => {
-	     assert.equal(dep[0],web3.toWei(0.001, "ether"), "amount deposit");
-	     assert.equal(dep[2],account_C, "exchanger saved correctly");
+	     assert.equal(dep[0],web3.toWei(0.0001, "ether"), "Check amount deposited");
 	}) 
 
-    });
+    }); 
 
 
     
 
-    /*Test 3: Withdraw*/
+    /*Test 3: Withdraw */
     it("Testing withdraw", function() {
-        return remi.depositOnContract(account_C, passHashed, {from: account_A, value: web3.toWei(0.1, "ether")})
-        .then(txObj => {
+        return remi.computeHash(account_A, pass1, pass2, account_C)
+	.then( _passHashed => {
+		passHashed = _passHashed;
+		return remi.depositOnContract(passHashed, duration, {from: account_A, value: web3.toWei(0.0001, "ether")})
+	}).then(txObj => {
              assert.equal(txObj.receipt.status, 1, "Deposit fails ");
-             assert.equal(txObj.logs.length,1, "deposit event has not been emitted");
-             assert.equal(txObj.logs[0].args.exchanger, account_C, "exchanger set correctly")
-             return remi.withdraw(pass1, pass2, {from: account_C})
+             assert.equal(txObj.logs.length,1, "Deposit event has not been emitted");
+             return remi.withdraw(pass1, pass2, account_A, {from: account_C})
         }).then(txObj => {
             assert.equal(txObj.receipt.status, 1, "Deposit fails ");
-            assert.equal(txObj.logs.length,1, "withdraw event has not been emitted");
-	    // in progress......
-	    assert.equal(txObj.logs[0].args.amount,web3.toWei(0.1, "ether"), "deposited and withdrawn amount is not the same");
+            assert.equal(txObj.logs.length,1, "Withdraw event has not been emitted");
+	    assert.equal(txObj.logs[0].args.amount,web3.toWei(0.0001, "ether"), "deposited and withdrawn amount is not the same");
 		
         })
 
     });
+
+    /*Test 4: Withdraw: only the exchanger can use this*/
+    it("Testing withdraw failure, permission denied", function() {
+        return remi.computeHash(account_A, pass1, pass2, account_C)
+	.then( _passHashed => {
+		passHashed = _passHashed;
+		return remi.depositOnContract(passHashed, duration, {from: account_A, value: web3.toWei(0.0001, "ether")})
+	}).then(txObj => {
+             assert.equal(txObj.receipt.status, 1, "Deposit fails ");
+             assert.equal(txObj.logs.length,1, "Deposit event has not been emitted");
+	     try{
+	     	remi.withdraw(pass1, pass2, account_A, {from: account_B})
+	     }catch(err){
+		console.log("Expected failure: " + err);
+		assert();
+		return;
+	     }
+	})
+
+    }); 
+
 
 })
